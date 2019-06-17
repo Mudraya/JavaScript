95a2f9e7d8e6a1c4a48edc41f3dd2d837e2bdb3a
Anastasiya Mudraya <mudraya.od@gmail.com>
2019-06-17 20:02:06 +0300

    zeit

--- index.js
+++ index.js
@@ -6,23 +6,19 @@
 // var fs = require('file-system');
 const TOKEN = process.env.TELEGRAM_TOKEN || '755380132:AAH326o9uguBRBOC9qpGX_n5TvQug85W8Ys'
 const TelegramBot = require('node-telegram-bot-api')
-const options = {
-  webHook: {
-    port: 443,
-    key: 'D:/FILES/J2019/node/telebot/key.pem',
-    cert: 'D:/FILES/J2019/node/telebot/server.crt'
-  }
-}
+// const options = {
+//   webHook: {
+//     port: 443
+//   }
+// }
 // This URL must route to the port set above (i.e. 443)
-const url = 'https://b77999ed.ngrok.io'
+const url = 'https://javascript.mudrayaod.now.sh/'
 const bot = new TelegramBot(TOKEN, { polling: true })
 
 // This informs the Telegram servers of the new webhook.
-bot.setWebHook(`${url}/bot${TOKEN}`, {
-  certificate: options.webHook.cert
-})
+bot.setWebHook(`${url}/bot${TOKEN}`)
 
 // Just to ping!
 bot.on('message', function onMessage (msg) {
-  bot.sendMessage(msg.chat.id, 'I am alive!')
+  bot.sendMessage(msg.chat.id, 'I am alive on Zeit Now!')
 })
