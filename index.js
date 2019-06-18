579255608c0e25d5d919ed1eff51fff1279da2a0
Anastasiya Mudraya <mudraya.od@gmail.com>
2019-06-18 18:48:25 +0300

    zeit18

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
-/**
- * This example demonstrates setting up a webook, using a
- * self-signed certificate.
- */
-
-// var fs = require('file-system');
-const TOKEN = '755380132:AAH326o9uguBRBOC9qpGX_n5TvQug85W8Ys'
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
+const http = require('http')
 const TelegramBot = require('node-telegram-bot-api')
-// const options = {
-//   webHook: {
-//     port: 443
-//   }
-// }
-// This URL must route to the port set above (i.e. 443)
+const token = process.env['tg_api_key'] || '755380132:AAH326o9uguBRBOC9qpGX_n5TvQug85W8Ys'
+const bot = new TelegramBot(token, { polling: true })
 const url = 'https://javascript.mudrayaod.now.sh'
-const bot = new TelegramBot(TOKEN, { polling: true })
 
-// This informs the Telegram servers of the new webhook.
-bot.setWebHook(`${url}/bot${TOKEN}`)
+bot.setWebHook(`${url}/bot${token}`)
 
-// Just to ping!
 bot.on('message', (msg) => {
-  bot.sendMessage(msg.from.id, 'I am alive on Zeit Now!')
+  // console.log(msg);
+  var fromId = msg.from.id
+  var sign = msg.text.toString().toLowerCase()
+
+  let optionsJsdom = {
+    referrer: 'http://astroscope.ru/horoskop/ejednevniy_goroskop/' + sign + '.html'
+  }
+
+  let request = http.get('http://astroscope.ru/horoskop/ejednevniy_goroskop/' + sign + '.html', function (response) {
+    if (response.statusCode === 200) {
+      JSDOM.fromURL('http://astroscope.ru/horoskop/ejednevniy_goroskop/' + sign + '.html', optionsJsdom).then(dom => {
+        let horoscope = dom.window.document.querySelectorAll('.p-3')[1].innerHTML
+        bot.sendMessage(fromId, horoscope)
+      }
+      )
+    } else { if (sign !== '/start' && sign !== '/help') bot.sendMessage(fromId, 'Пожалуйста, придерживайся инструкции ;)') }
+  })
+
+  request.on('error', function (error) {
+    console.error(error.status)
+  })
+})
+
+bot.onText(/\/(start|help)/, function (msg, match) {
+  var fromId = msg.from.id
+
+  if (match[1].toString() === 'start') {
+    bot.sendMessage(fromId, 'Добро пожаловать!\nЧтобы узнать как пользоваться ботом используй команду /help')
+  }
+
+  if (match[1].toString() === 'help') {
+    bot.sendMessage(fromId, 'Давай же вместе узнаем, что интересного ждет тебя сегодня)\n' +
+      'Надеюсь, ты знаешь свой зодиакальный знак) В зависимости от этого введи одно из следующих слов:\n\n' +
+      'aries - если ты Овен\n' +
+      'taurus - если ты Телец\n' +
+      'gemini - если ты Близнецы\n' +
+      'cancer - если ты Рак\n' +
+      'leo - если ты Лев\n' +
+      'virgo - если ты Дева\n' +
+      'libra - если ты Весы\n' +
+      'scorpio - если ты Скорпион\n' +
+      'sagittarius - если ты Стрелец\n' +
+      'capricorn - если ты Козерог\n' +
+      'aquarius - если ты Водолей\n' +
+      'pisces - если ты Рыбы\n')
+  }
 })
